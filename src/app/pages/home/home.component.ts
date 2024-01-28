4c81a351df5514b418888eccd1ad788747eeb973
Eugene Santos <Eugene_Sergio_Castro_DeLos_Santos@manulife.com>
2024-01-29 01:10:57 +0800

    navbar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CategoriesComponent } from '../../modules/categories/categories.component';
 import { ActionsComponent } from '../../modules/actions/actions.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CategoriesComponent, ActionsComponent],
+  imports: [CategoriesComponent, ActionsComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
